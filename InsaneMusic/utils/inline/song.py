7e6ac4b0ce4c3ccaba6a8ee0d317f17e70d4f05f
musicharshita <143814471+musicharshita@users.noreply.github.com>
2023-09-02 17:08:24 +0530

    Update song.py

--- InsaneMusic/utils/inline/song.py
+++ InsaneMusic/utils/inline/song.py
@@ -19,7 +19,7 @@ def song_markup(_, vidid):
         [
             InlineKeyboardButton(
                 text="• sᴜᴩᴩᴏʀᴛ •",
-                url="https://t.me/Dosto_ki_Mehfil786",
+                url="https://t.me/FEELING_GANA",
             ),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
